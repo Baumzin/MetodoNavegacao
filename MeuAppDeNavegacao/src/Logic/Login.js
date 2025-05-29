9cb9273acd1f3c5f0396e689d626728931346b38
BRENO VALENTIM BERNARDO <breno.bernardo01@etec.sp.gov.br>
2025-05-29 17:40:48 -0300

    Começando atividade de armazenamento no dispositivo

--- MeuAppDeNavegacao/src/Logic/Login.js
+++ MeuAppDeNavegacao/src/Logic/Login.js
@@ -1,5 +1,46 @@
+import * as React from 'react';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function ValidarLogin(usuario, senha, navigation ){
+    const [login, setLogin] = useState("");
+    const [logins, setLogins] = useState("");
+  
+    //função para salvar os logins no AsyncStorage
+    const salvarLogins = async (loginsArray) => {
+      try {
+        await AsyncStorage.setItem('logins', JSON.stringify(loginsArray));
+      } catch (error){
+        console.log("Erro ao efeutar o login: ", error);
+      }
+    };
+  
+    //função para carregar tarefas ao iniciar o app
+    const carregarLogins = async () => {
+      try {
+        const loginsArmazenados = await AsyncStorage.getItem('logins');
+        if(loginsArmazenados !== null){
+          setTasks(JSON.parse(loginsArmazenados));
+        }
+      } catch(error) {
+        console.log("Erro ao carregar os logins: ", error);
+      }
+    }
+  
+    //função que adiciona um novo login
+    const adicionaLogin = () => {
+      if(task.trim() !== ""){
+        const novosLogins = [...logins, login];
+        setLogins(novosLogins);
+        salvarLogins(novosLogins); //salva a lista atualizada
+        setLogin(""); //limpa o campo de entrada
+      }
+    };
+  
+    //carrega os logins ao iniciar o aplicativo
+    useEffect(() => {
+      carregarLogins();
+    }, []);
+  
     if(usuario == "admin" && senha == "1234"){
         navigation.navigate('HomeScreen');
         
